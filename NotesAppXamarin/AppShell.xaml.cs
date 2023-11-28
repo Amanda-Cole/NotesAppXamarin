d38fc2115878de030c89a87fc90193798beaa0d5
Amanda Cole <col22031@byui.edu>
2023-11-28 09:44:36 -0700

    Add project files.

---
+++ NotesAppXamarin/AppShell.xaml.cs
@@ -0,0 +1,14 @@
+﻿using NotesAppXamarin.Views;
+using Xamarin.Forms;
+
+namespace NotesAppXamarin
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(NoteEntryPage), typeof(NoteEntryPage));
+        }
+    }
+}
